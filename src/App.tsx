e8611b57f88fd40b4577ff7f15f7973067a30450
malikaiyer <malikakarthik@gmail.com>
2023-08-30 20:02:38 -0400

    added hello world

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript - Malika Iyer
+                UD CISC275 with React Hooks and TypeScript - Malika Iyer - Hello
+                World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
