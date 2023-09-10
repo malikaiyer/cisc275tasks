20e3ff08fae4d474eb606e1638399f8ad74d3e29
malika <malikakarthik@gmail.com>
2023-09-09 22:49:59 -0400

    completed two functions

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,8 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    const cel: number = (temperature - 32) * (5 / 9);
+    return cel;
 }
 
 /**
@@ -12,7 +13,17 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let sum1 = 0; //got an error when I added sum: number to this/ it said "Type number trivially inferred from a number literal, remove type annotation."
+    if (first > 0) {
+        sum1 = sum1 + first;
+    }
+    if (second > 0) {
+        sum1 = sum1 + second;
+    }
+    if (third > 0) {
+        sum1 = sum1 + third;
+    }
+    return sum1;
 }
 
 /**
