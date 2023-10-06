5fd3db3d10d1a59a5f684714a8cec195eefe3c80
malika <malikakarthik@gmail.com>
2023-10-06 11:56:36 -0400

    all but one function complete

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,12 +1,32 @@
+/* eslint-disable brace-style */
+/* eslint-disable prettier/prettier */
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [ans, setAns] = useState<string>("");
+    const [displayText, setDisplayText] = useState<string>("❌");
+
+    function updateAns(event: React.ChangeEvent<HTMLInputElement>) {
+        setAns(event.target.value);
+        if(event.target.value===expectedAnswer){
+            setDisplayText("✔️");
+        }
+        else{
+            setDisplayText("❌");
+        }
+    }
     return (
         <div>
+            <Form.Group controlId="answer">
+                <Form.Label>Answer:</Form.Label>
+                <Form.Control value={ans} onChange={updateAns} />
+            </Form.Group>
+            <div>The answer is: {displayText}</div>
             <h3>Check Answer</h3>
         </div>
     );
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,58 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [editOn, setEditOn] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+
+    // Function to toggle edit mode
+    function toggleEditMode() {
+        setEditOn(!editOn);
+    }
+
+    // Function to handle name input change
+    function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value);
+    }
+
+    // Function to handle student checkbox change
+    function handleStudentChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsStudent(event.target.checked);
+    }
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="edit-mode-check"
+                checked={editOn}
+                onChange={toggleEditMode}
+            />
+            <div>
+                {name} is {isStudent ? "a student" : "not a student"}
+            </div>
+            {editOn && (
+                <div>
+                    <Form.Group controlId="name">
+                        <Form.Label>Edit Name:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={name}
+                            onChange={handleNameChange}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="student">
+                        <Form.Check
+                            type="checkbox"
+                            label="Is a student"
+                            checked={isStudent}
+                            onChange={handleStudentChange}
+                        />
+                    </Form.Group>
+                </div>
+            )}
         </div>
     );
 }
